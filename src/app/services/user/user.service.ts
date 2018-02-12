6abbf6d1e17775c360526a4f3f97107e7ee4738a
Gabriel Katz <gkatz@gmx.net>
2018-02-12 20:27:31 +0100

    List Users from Backend in list.

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 export interface User {
   name: string;
@@ -7,11 +9,19 @@ export interface User {
 @Injectable()
 export class UserService {
   public user: User;
+  private collection: AngularFirestoreCollection<User>;
+  private _users: Observable<User[]>;
 
-  constructor() { }
+  constructor(private db: AngularFirestore) {
+    this.collection = db.collection<User>('user', ref => ref.orderBy('name'));
+    this._users = this.collection.valueChanges();
+   }
 
   public set(user: User) {
     this.user = user;
   }
 
+  public list() {
+    return this._users;
+  }
 }
