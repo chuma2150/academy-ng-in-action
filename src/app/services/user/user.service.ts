3389baa090332f4b287206ce8bea19a5a3682981
derEisbaer <emil@effila.com>
2018-02-12 20:49:57 +0100

    added user component

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,5 +1,8 @@
+import { Observable } from 'rxjs/Observable';
 import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subject } from 'rxjs/Subject';
 
 export interface User {
   name: string;
@@ -8,14 +11,19 @@ export interface User {
 @Injectable()
 export class UserService {
   private collection: AngularFirestoreCollection<User>;
-  public user: User;
+  private user$: Subject<User> = new BehaviorSubject<User>({name: 'No user'});
 
   constructor(private store: AngularFirestore) {
       this.collection = store.collection('user');
   }
 
   public set(user: User) {
-    this.user = user;
+    this.user$.next(user);
     this.collection.add(user);
   }
+
+  public user(): Observable<User> {
+    return this.user$.asObservable();
+  }
+
 }
