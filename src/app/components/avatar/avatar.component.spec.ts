742528816cd2ddd6a0e7f1fc0670c0178a6ad512
Gabriel Katz <gkatz@gmx.net>
2018-02-20 20:50:36 +0100

    Add unit test for avatar component.

--- src/app/components/avatar/avatar.component.spec.ts
+++ src/app/components/avatar/avatar.component.spec.ts
@@ -1,20 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AvatarComponent } from './avatar.component';
+import { By } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
+@Component({
+  selector: 'app-test-cmpt',
+  template: `<app-avatar [name]="name"></app-avatar>`
+})
+class TestComponent {
+  name: string;
+}
 describe('AvatarComponent', () => {
-  let component: AvatarComponent;
-  let fixture: ComponentFixture<AvatarComponent>;
-
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AvatarComponent ]
+      declarations: [ TestComponent, AvatarComponent ]
     })
     .compileComponents();
   }));
 
    beforeEach(() => {
-     fixture = TestBed.createComponent(AvatarComponent);
+     fixture = TestBed.createComponent(TestComponent);
      component = fixture.componentInstance;
      fixture.detectChanges();
    });
@@ -22,4 +30,12 @@ describe('AvatarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should set the correct background url', async(() => {
+    const name = 'Hans';
+    component.name = name;
+    fixture.detectChanges();
+    const iconStyle = fixture.debugElement.query(By.css('i')).styles;
+    expect(iconStyle['background-image']).toEqual(`url(https://api.adorable.io/avatars/40/${name}.png)`);
+  }));
 });
