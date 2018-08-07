01ace6777316ed000addd23532d76d922cb9859e
Gabriel Katz <gkatz@gmx.net>
2018-08-07 22:14:20 +0200

    fix production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCsfF64JECmI1fulUXYIerv6M49IEd-DbQ',
+    authDomain: 'ng-in-action.firebaseapp.com',
+    databaseURL: 'https://ng-in-action.firebaseio.com',
+    projectId: 'ng-in-action',
+    storageBucket: 'ng-in-action.appspot.com',
+    messagingSenderId: '911136048101'
+  }
 };
