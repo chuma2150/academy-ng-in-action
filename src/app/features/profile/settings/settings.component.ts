ec74be9561b78b5de9c194e4b532f67f830b2580
Martin Bundschuh <martin.bundschuh@ti8m.ch>
2021-09-07 17:59:09 +0200

    Add haircolors as type

---
+++ src/app/features/profile/settings/settings.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, pluck } from 'rxjs/operators';
+import { HairColors, User, UserService } from 'src/app/services/user/user.service';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.scss']
+})
+export class SettingsComponent implements OnInit {
+  public currentProfile$: Observable<User>;
+  public availableHairColors = HairColors;
+  
+  constructor(private route: ActivatedRoute, private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.currentProfile$ = this.route.data.pipe(
+      pluck('user'),
+      map(u =>  Object.assign({}, u)),
+    );
+  }
+
+  save(updatedProfile: User): void {
+    console.log(updatedProfile);
+    this.userService.update(updatedProfile);
+  }
+}
