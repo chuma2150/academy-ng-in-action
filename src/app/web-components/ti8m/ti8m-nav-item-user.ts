98995e06599c0f1841c1e1a969bb27f101bbb4d4
Martin Bundschuh <martin.bundschuh@ti8m.ch>
2021-09-07 17:50:39 +0200

    Fix hair color apearance

--- src/app/web-components/ti8m/ti8m-nav-item-user.ts
+++ src/app/web-components/ti8m/ti8m-nav-item-user.ts
@@ -50,7 +50,7 @@ export class Ti8mNavItemUserCustomComponent extends LitElement {
   }
 
   getUrl(avatar: string) {
-    return `https://api.adorable.io/avatars/human/${avatar}.svg`;
+    return `https://avatars.dicebear.com/api/human/${avatar}.svg`;
   }
 
   render() {
