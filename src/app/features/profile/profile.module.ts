192f14c39817ea54900f13930e7797343922b3bc
Martin Bundschuh <martin.bundschuh@ti8m.ch>
2021-09-07 17:04:53 +0200

    Add initial form with name field

--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -2,11 +2,15 @@ import { ListModule } from './../list/list.module';
 import { MatCardModule } from '@angular/material/card';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 import { ComponentsModule } from '../../components/components.module';
 import { SettingsComponent } from './settings/settings.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
@@ -14,7 +18,11 @@ import { SettingsComponent } from './settings/settings.component';
     MatCardModule,
     ComponentsModule,
     ListModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   declarations: [ProfileComponent, SettingsComponent]
 })
--- src/app/features/profile/settings/settings.component.ts
+++ src/app/features/profile/settings/settings.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { pluck } from 'rxjs/operators';
-import { User } from 'src/app/services/user/user.service';
+import { map, pluck } from 'rxjs/operators';
+import { User, UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-settings',
@@ -12,9 +12,16 @@ import { User } from 'src/app/services/user/user.service';
 export class SettingsComponent implements OnInit {
   public currentProfile$: Observable<User>;
   
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private userService: UserService) { }
 
   ngOnInit(): void {
-    this.currentProfile$ = this.route.data.pipe(pluck('user'))
+    this.currentProfile$ = this.route.data.pipe(
+      pluck('user'),
+      map(u =>  Object.assign({}, u)),
+    );
+  }
+
+  save(updatedProfile: User): void {
+    this.userService.update(updatedProfile);
   }
 }
