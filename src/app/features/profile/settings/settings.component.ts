20a38efd088876e08bd27a4533603af965cbec31
Gabriel Katz <gabriel.katz@ti8m.ch>
2021-09-07 18:43:01 +0200

    Add snackbar for updated user.

--- src/app/features/profile/settings/settings.component.ts
+++ src/app/features/profile/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, pluck } from 'rxjs/operators';
@@ -13,7 +14,7 @@ export class SettingsComponent implements OnInit {
   public currentProfile$: Observable<User>;
   public availableHairColors = HairColors;
   
-  constructor(private route: ActivatedRoute, private userService: UserService) { }
+  constructor(private route: ActivatedRoute, private userService: UserService, private snackbar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.currentProfile$ = this.route.data.pipe(
@@ -23,6 +24,6 @@ export class SettingsComponent implements OnInit {
   }
 
   save(updatedProfile: User): void {
-    this.userService.update(updatedProfile).subscribe();
+    this.userService.update(updatedProfile).subscribe(() => this.snackbar.open('User updated!', undefined, { duration: 5000 }));
   }
 }
