71b80b0acb3ed169f272bfc472d3cf9734dbc35e
Martin Bundschuh <martin.bundschuh@ti8m.ch>
2021-09-08 14:02:20 +0200

    Remove unecessary async pipes

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -9,10 +9,10 @@ import {Router} from '@angular/router';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
-  user: Observable<User | null>;
+  user$: Observable<User | null>;
 
   constructor(private router: Router, private userService: UserService) {
-    this.user = this.userService.user();
+    this.user$ = this.userService.user();
   }
 
   public logout() {
