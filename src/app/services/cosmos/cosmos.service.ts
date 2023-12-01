82bec55757a6503104fa4f6ca9fa1e7dc574e9cd
Martin Bundschuh <chuma2150@gmail.com>
2023-12-01 16:06:10 +0100

    Add missing service and revert async pipe change

---
+++ src/app/services/cosmos/cosmos.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { Container, CosmosClient, Database } from "@azure/cosmos";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CosmosService {
+  private readonly client: CosmosClient;
+  private readonly db: Database;
+  readonly users: Container;
+  readonly messages: Container;
+
+  constructor() {
+    this.client = new CosmosClient(environment.cosmos);
+
+    this.db = this.client.database('academyNgInActionDB');
+
+    this.users = this.db.container('usersCollection');
+    this.messages = this.db.container('messagesCollection');
+  }
+}
