fc7cb29e64bf8f339444ce76b9515f190cb84ab4
Martin Bundschuh <chuma2150@gmail.com>
2023-12-06 11:42:57 +0100

    Use azure hosted api and use proper azure credentials for cosmos db access

--- api/Cosmos/CosmosClientFactory.cs
+++ api/Cosmos/CosmosClientFactory.cs
@@ -6,6 +6,6 @@ namespace Api.Cosmos;
 public class CosmosClientFactory : ICosmosClientFactory
 {
     public CosmosClient Create() => new(
-        "https://academynginaction.documents.azure.com",
-        "OwS7XILmZR7XTqOZpi0mASBLhLOBEWCxWcECId85P3hG3apUIeKgQKAIaKknwROb1F562KcipRwYACDbVkdLbA==");
+        Environment.GetEnvironmentVariable("COSMOS_ENDPOINT"),
+        Environment.GetEnvironmentVariable("COSMOS_KEY"));
 }
--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  endpoint: 'https//localhost:7157',
+  // If BE is hosted locally (dotnet run) then use http://localhost:5125 or https//localhost:7157.
+  endpoint: 'https://academy-ng-in-action-api.azurewebsites.net',
 };
