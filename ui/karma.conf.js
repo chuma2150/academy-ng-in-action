07cc9c15474d47ea0f51486082ca02865446af48
Martin Bundschuh <chuma2150@gmail.com>
2023-12-10 13:00:03 +0100

    Remove unnecessary packages and adapt karam conf

--- ui/karma.conf.js
+++ ui/karma.conf.js
@@ -7,17 +7,16 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-safari-launcher'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, 'coverage'), reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     reporters: ['coverage-istanbul', 'progress', 'kjhtml'],
