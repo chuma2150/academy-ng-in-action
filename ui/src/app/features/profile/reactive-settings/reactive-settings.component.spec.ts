6269ad9ad5d462e9e8bb2b1d4ddad62a3862c6ed
Sebastian Renner <Sebastian.Renner@ti8m.ch>
2023-12-22 11:30:37 +0100

    Fixed unit test

--- ui/src/app/features/profile/reactive-settings/reactive-settings.component.spec.ts
+++ ui/src/app/features/profile/reactive-settings/reactive-settings.component.spec.ts
@@ -1,5 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AgePipe } from 'src/app/components/profile-view';
 import { ReactiveSettingsComponent } from './reactive-settings.component';
 
 describe('ReactiveSettingsComponent', () => {
@@ -8,10 +19,32 @@ describe('ReactiveSettingsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ReactiveSettingsComponent],
+      declarations: [
+        ReactiveSettingsComponent,
+        AgePipe,
+      ],
+      imports: [
+        RouterModule.forRoot([]),
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatSnackBarModule,
+        MatCardModule,
+        MatOptionModule,
+        MatNativeDateModule,
+        MatDatepickerModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatSelectModule,
+      ],
+      providers: [
+        provideAnimations(),
+      ],
     })
-    .compileComponents();
+      .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ReactiveSettingsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
