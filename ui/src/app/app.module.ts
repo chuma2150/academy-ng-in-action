f91992b9a34cc0af021103a20fe03071e7d1d744
Sebastian Renner <Sebastian.Renner@ti8m.ch>
2024-02-16 14:17:01 +0100

    Added store dev tools

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { ComponentsModule } from './components/components.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, isDevMode } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
@@ -12,6 +12,7 @@ import { FeaturesModule } from './features';
 import { LoggingInterceptor } from './interceptors/logging.interceptor';
 import { StoreModule } from '@ngrx/store';
 import { userReducer } from './state/user/user.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent],
@@ -25,6 +26,7 @@ import { userReducer } from './state/user/user.reducer';
     HttpClientModule,
     MatNativeDateModule,
     StoreModule.forRoot({ user: userReducer}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
