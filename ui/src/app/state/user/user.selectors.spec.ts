86263abf0be3d33e860b426c49d5d18570927f75
Sebastian Renner <Sebastian.Renner@ti8m.ch>
2024-02-16 14:58:18 +0100

    Added unit tests for user reducer and selector

---
+++ ui/src/app/state/user/user.selectors.spec.ts
@@ -0,0 +1,23 @@
+import { User } from "src/app/services";
+import { UserState } from "./user.reducer";
+import { selectUser } from "./user.selectors";
+
+interface AppState {
+  user: UserState
+}
+
+describe('User Selectors', () => {
+  it('should return the user from the state', () => {
+    const user: User = {
+      name: 'Max'
+    };
+    const appState: AppState = {
+      user: {
+        user: user
+      }
+    };
+
+    const result = selectUser(appState);
+    expect(result).toEqual(user);
+  })
+});
