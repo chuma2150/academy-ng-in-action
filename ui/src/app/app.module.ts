df27bfda422f7a9d2b6e882b402df69f27f46c64
Martin M <chuma2150@gmail.com>
2024-03-14 10:35:51 +0100

    Fix merge conflit

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,8 +1,4 @@
 import { ComponentsModule } from './components/components.module';
-<<<<<<< HEAD
-=======
-import { BrowserModule } from '@angular/platform-browser';
->>>>>>> solution
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule, isDevMode } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -11,18 +7,12 @@ import { MatNativeDateModule } from '@angular/material/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { LoggingInterceptor } from './interceptors/logging.interceptor';
 import { StoreModule } from '@ngrx/store';
 import { userReducer } from './state/user/user.reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserComponent } from './components/user';
-=======
-import { FeaturesModule } from './features';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
->>>>>>> solution
 
 @NgModule({
   declarations: [AppComponent],
@@ -34,12 +24,8 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     ComponentsModule,
     HttpClientModule,
     MatNativeDateModule,
-<<<<<<< HEAD
     UserComponent,
     StoreModule.forRoot({ user: userReducer }),
-=======
-    StoreModule.forRoot({}),
->>>>>>> solution
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],
   providers: [{
